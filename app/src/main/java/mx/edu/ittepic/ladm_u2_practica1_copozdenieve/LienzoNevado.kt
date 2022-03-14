fb49390df557bee4cbb817b5024697b0c8fed132
Carlos Uriel Fregoso Espericueta <caurfregosoes@ittepic.edu.mx>
2022-03-14 13:04:33 -0700

    primer ovalo

--- app/src/main/java/mx/edu/ittepic/ladm_u2_practica1_copozdenieve/LienzoNevado.kt
+++ app/src/main/java/mx/edu/ittepic/ladm_u2_practica1_copozdenieve/LienzoNevado.kt
@@ -12,16 +12,19 @@ class LienzoNevado(ActMain : MainActivity) : View(ActMain) {
 
     //inicio del metodo onDraw
     override fun onDraw(c: Canvas) {
-
-        var p = Paint()
-        var equis : Float = x
-        var ye: Float = y
-        actPrincipal.setTitle("Ancho: ${equis}, Alto: ${ye}")
-
-        p.setColor(Color.GREEN)
-
-
-
         super.onDraw(c)
+        var p = Paint()
+        var ajusteAlto = 140f
+        actPrincipal.setTitle("Ancho: ${width}, Alto: ${height}")
+
+        c.drawColor(Color.WHITE)
+        var colorPiso = Color.rgb(108,79,79)
+        p.setColor(colorPiso)
+        //c.drawCircle(280f,640f,3400f,p)
+        c.drawOval(-140f,290+ajusteAlto,400f,720+ajusteAlto,p)
+
+
+
+
     }
 }
